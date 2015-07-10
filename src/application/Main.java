f52ebca3cd736515dc513740d3bf6b931796f7e0
Dennis Schwarz <dennis.s.schwarz@gmail.com>
2015-07-10 14:18:35 +0200

    Adding Sphere and Parallel Transition

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,20 +1,49 @@
 package application;
 	
+import javafx.animation.ParallelTransition;
+import javafx.animation.Timeline;
+import javafx.animation.TranslateTransition;
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.shape.Sphere;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
+			Group group = new Group();
+			Sphere sphere = new Sphere(20);
+			
+			
+			TranslateTransition translateTransitionX = new TranslateTransition(Duration.millis(2000), sphere);
+			    translateTransitionX.setFromX(50);
+			    translateTransitionX.setToX(350);
+			    translateTransitionX.setCycleCount(1);
+			    translateTransitionX.setAutoReverse(false);
+			
+		    TranslateTransition translateTransitionY = new TranslateTransition(Duration.millis(2000), sphere);
+			    translateTransitionY.setFromY(50);
+			    translateTransitionY.setToY(350);
+			    translateTransitionY.setCycleCount(1);
+			    translateTransitionY.setAutoReverse(false);
+			
+			    
+		    ParallelTransition parallelTransition = new ParallelTransition();
+		    parallelTransition.getChildren().addAll(translateTransitionX, translateTransitionY);
+		    parallelTransition.setCycleCount(1);
+		    parallelTransition.play();
+
+		    group.getChildren().add(sphere);
+			
+		    Scene scene = new Scene(group, 400, 400);
+		    scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		    primaryStage.setScene(scene);
+		    primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
