5a7bdc8814e49b87ff66450b62f7fdaac55061b3
Dennis Schwarz <dennis.s.schwarz@gmail.com>
2015-07-10 14:19:58 +0200

    Removed Unused Imports, clean Project

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,14 +1,12 @@
 package application;
 	
 import javafx.animation.ParallelTransition;
-import javafx.animation.Timeline;
 import javafx.animation.TranslateTransition;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.shape.Sphere;
 
 
